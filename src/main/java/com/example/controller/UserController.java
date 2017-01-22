0502f4c57c6861d5021a599c7634ef0ae5753e34
cy02 <cy02@MIS-CY02.expopay.corp>
2017-01-22 15:18:44 +0800

    增加注解注释说明

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.User;
 import com.example.service.UserService;
+
+/*
+ * @RestController:等价于@Controller+@ResponseBody
+ * 1)如果只是使用@RestController注解Controller，则Controller中的方法无法返回jsp页面，配置的视图解析器InternalResourceViewResolver不起作用，返回的内容就是Return 里的内容。
+ * 例如：本来应该到success.jsp页面的，则其显示success.
+ * 2)如果需要返回到指定页面，则需要用 @Controller配合视图解析器InternalResourceViewResolver才行。
+ * 3)如果需要返回JSON，XML或自定义mediaType内容到页面，则需要在对应的方法上加上@ResponseBody注解。
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
